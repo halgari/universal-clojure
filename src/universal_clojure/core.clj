49ccedf6119227702abf0510a06e4802371877c9
Timothy Baldridge <tbaldridge@gmail.com>
2012-06-05 20:42:42 -0500

    first import

---
+++ src/universal_clojure/core.clj
@@ -0,0 +1,1 @@
+(ns universal-clojure.core)
---
+++ test/universal_clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns universal-clojure.test.core
+  (:use [universal-clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
